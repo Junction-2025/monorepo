3e5d45b745ad844132cf7937fd2781ac29e37850
Miika Piiparinen <100348027+MiikaMatias@users.noreply.github.com>
2025-11-14 18:52:16 +0200

    uv

---
+++ pyntcloud-implementation/README.md
@@ -0,0 +1,2 @@
+# Pyntcloud solution
+This solution is an implementation of pyntcloud for the challenge
---
+++ pyntcloud-implementation/main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from pyntcloud!")
+
+
+if __name__ == "__main__":
+    main()
