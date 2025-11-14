4a9dbe803d22ba07bdc46217900fe912fd6cbe3c
Miika Piiparinen <100348027+MiikaMatias@users.noreply.github.com>
2025-11-14 20:48:42 +0200

    fix ass

--- pyntcloud-implementation/src/config.py
+++ pyntcloud-implementation/src/config.py
@@ -2,7 +2,7 @@ from pathlib import Path
 
 ROOT_DIR = Path(__file__).parent.parent.parent
 LOG_DIR = ROOT_DIR / 'logs'
-DATA_DIR = ROOT_DIR / "data"
+DATA_DIR = ROOT_DIR / 'pyntcloud-implementation' / "data"
 
 BASE_WIDTH = 1280
 BASE_HEIGHT = 720
