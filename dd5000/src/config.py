6036df2812c24780c6dcc08dd66f925c4452ff76
Miika Piiparinen <100348027+MiikaMatias@users.noreply.github.com>
2025-11-15 15:14:56 +0200

    init gs

---
+++ dd5000/src/config.py
@@ -0,0 +1,11 @@
+from pathlib import Path
+
+BATCH_WINDOW_US = 150_000
+BASE_DIR = Path(__file__).parent.parent
+LOG_DIR =  BASE_DIR / "logs"
+LOG_DIR.mkdir(exist_ok=True)
+
+BASE_WIDTH = 1280
+BASE_HEIGHT = 720
+
+YOLO_CONFIDENCE_THRESHOLD = 0.2
---
+++ dd5000/src/main.py
@@ -0,0 +1,73 @@
+import argparse
+from pathlib import Path
+
+from src.config import BATCH_WINDOW_US, BASE_WIDTH, BASE_HEIGHT
+
+from src.evio_lib.pacer import Pacer
+from src.evio_lib.dat_file import DatFileSource
+from src.evio_lib.play_dat import get_frame, get_window
+
+from src.yolo import detect_drone_crop
+from src.logger import get_logger
+
+
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(
+        description="Stream a .dat file with real-time pacing."
+    )
+    parser.add_argument(
+        "--input",
+        type=Path,
+        required=True,
+        help="Input .dat file path.",
+    )
+    parser.add_argument(
+        "--speed",
+        type=float,
+        default=1.0,
+        help="Playback speed multiplier (1.0 = real-time).",
+    )
+    parser.add_argument(
+        "--window",
+        type=int,
+        default=BATCH_WINDOW_US,
+        help=f"Batch window size in microseconds (default: {BATCH_WINDOW_US}µs).",
+    )
+
+    return parser.parse_args()
+
+
+
+def main():
+    args = parse_args()
+
+    src = DatFileSource(
+        args.input,
+        width=BASE_WIDTH,
+        height=BASE_HEIGHT,
+        window_length_us=args.window,
+    )
+    pacer = Pacer(
+        speed=args.speed,
+        force_speed=args.force_speed,
+        drop_tolerance_s=args.drop_tolerance,
+    )
+
+    logger = get_logger()
+
+    logger.info("\n--- Data Import Complete ---")
+    for batch_range in pacer.pace(src.ranges()):
+        window = get_window(
+            src.event_words,
+            src.order,
+            batch_range.start,
+            batch_range.stop,
+        )
+        frame = get_frame(window)
+        yolo_bounding_box = detect_drone_crop(frame)
+
+
+
+
+if __name__ == "__main__":
+    main()
