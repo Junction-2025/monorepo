749f316ef163dff902b1ac6adfb1486f93b2ca29
Miika Piiparinen <100348027+MiikaMatias@users.noreply.github.com>
2025-11-15 03:54:08 +0200

    outlier threshold

--- predictor/src/roo/config.py
+++ predictor/src/roo/config.py
@@ -4,4 +4,4 @@ HEATMAP_PIXEL_SIZE = 1
 EPSILON = 0.3  # Threshold factor for centroid selection
 MAX_ITERATIONS = 100  # Maximum K-means iterations
 CONVERGENCE_THRESHOLD = 1e-4  # Centroid movement threshold for convergence
-OUTLIER_THRESHOLD_MULTIPLIER = 1  # Threshold multiplier for outlier detection
+OUTLIER_THRESHOLD_MULTIPLIER = 3  # Threshold multiplier for outlier detection
